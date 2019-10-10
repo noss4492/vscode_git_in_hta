02c02e3a1e72c5634bbb60d18f5e3f1983cc9a41
noss <noss4492@gmail.com>
2019-10-10 19:37:33 +0900

    day off

---
+++ D20191010_0_java_summary_by_noss.java
@@ -0,0 +1,36 @@
+// 오늘부터 자바다!!
+
+// suns, 가전제품 멀티 플랫폼 코딩 빡침, 플랫폼 독립적, 제임스 고슬링이 놀면서 만듬
+
+// 아 벌써 java 유료화 되었구나 그래서 코틀린이 만들어졌구나
+
+/*
+java 환경설정
+1. Java SE 8u221 설치
+2. 환경변수 설정 (path에 추가 C:\Program Files\Java\jdk1.8.0_221\bin)
+path.. 고통받았었었음...
+*/
+
+/*
+Z: cd
+Z:\>dir
+ Z 드라이브의 볼륨: Free_for_work
+ 볼륨 일련 번호: 96CE-8725
+
+ Z:\ 디렉터리
+
+2019-10-10  오후 05:50               415 Hello.class
+2019-10-10  오후 05:26               107 Hello.java
+               2개 파일                 522 바이트
+               0개 디렉터리      91,439,104 바이트 남음
+
+Z:\>java Hello
+Hello World
+
+IDE
+netBeans
+Eclipes
+intellij
+
+
+*/
