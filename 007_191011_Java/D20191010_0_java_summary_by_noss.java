14133f1184769ac60598123267c2dd44ef4d2c3f
noss <noss4492@gmail.com>
2019-10-11 10:02:00 +0900

    ㅗㅡㅡ

---
+++ 007_191011_Java/D20191010_0_java_summary_by_noss.java
@@ -0,0 +1,54 @@
+// 오늘부터 자바다!!
+
+// suns, 가전제품 멀티 플랫폼 코딩 빡침, 플랫폼 독립적, 제임스 고슬링이 놀면서 만듬
+
+// 아 벌써 java 유료화 되었구나 그래서 코틀린이 만들어졌구나
+
+/*
+java 환경설정
+1. Java SE 8u221 설치
+2. 환경변수 설정 
+(변수 추가 : path에 추가 C:\Program Files\Java\jdk1.8.0_221\bin)
+(변수 추가 : CLASSPATH 값.)
+path.. 고통받았었었음...
+eclipse-jee-2019-09-R-win32-x86_64.zip 받자 (엔터프라이즈)
+
+헐 인텔리j랑 파이참 에듀케이션버전 공짜임
+
+IDE
+netBeans
+Eclipes
+intellij
+
+
+*/
+
+/*
+Z: cd
+Z:\>dir
+ Z 드라이브의 볼륨: Free_for_work
+ 볼륨 일련 번호: 96CE-8725
+
+ Z:\ 디렉터리
+
+2019-10-10  오후 05:50               415 Hello.class
+2019-10-10  오후 05:26               107 Hello.java
+               2개 파일                 522 바이트
+               0개 디렉터리      91,439,104 바이트 남음
+
+Z:\>java Hello
+Hello World
+
+
+
+*/
+
+
+package basic;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+//
